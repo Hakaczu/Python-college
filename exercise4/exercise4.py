8466f77353e9ddc385a30266802ee39569a9028b
cssrumi <33814533+cssrumi@users.noreply.github.com>
2018-11-27 15:44:08 +0100

    Update exercise4.py
    Małe wskazówki

--- exercise4/exercise4.py
+++ exercise4/exercise4.py
@@ -1,15 +1,41 @@
 import os
+import datetime
+// ogólnie nie ma żadnej walidacji tego co ktoś wpisze xd
+// directory = input("Directory: ")
 directory = input("Directory: ")
-file = input("Name of file: ")
-path = directory + "/" + file + ".txt"
+while not os.path.isdir(folder) and folder != '':
+    print('invalid folder, try again')
+    directory = str(input("Directory: "))
+
+// według polecenia nazwa pliku powinna być datą
+// file = input("Name of file: ")
+date = datetime.datetime.now()
+file = str(date).replace(':', '') + '.txt'
+// ścieżka jest inna pod windowsem a inna pod unixem/linuxem dlatego lepiej użyć os.path.join()
+// path = directory + "/" + file + ".txt"
+path = os.path.join(directory, file + ".txt")
 execute = "notepad.exe " + path
-try:
-    os.stat(directory)
-except:
-    os.mkdir(directory)
+
+// sprawdzasz czy istnieje coś co ma nazwę directory i czy da się to czytać - nie sprawdzasz czy jest folderem
+// https://docs.python.org/2/library/stat.html
+// try:
+//     os.stat(directory)
+// except:
+//     os.mkdir(directory)
+if not os.path.isdir(directory):
+    try:
+        os.mkdir(directory)
+    except Exception as e:
+        print(e)
+        
 os.system(execute)
 count = len(open(path).readlines())
 
+// obczaj coś takiego jak with open() - po zakończeniu bloku nie trzeba pamiętać o .close()
+// np.
+// if counter == 0:
+//     with open("puste.txt", 'a+') as f:
+//         f.write(path + '\n')
 if count == 0:
     print("puste")
     f = open("puste.txt", "a+")
